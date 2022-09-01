bdc3b9474d2ee157ba469b07a6dff7f442d31ed0
JudeJeanPierre <110211488+JudeJeanPierre@users.noreply.github.com>
2022-08-31 22:11:42 -0500

    Battling game - 2nd commit

--- OneDrive/Bureau/RTT25/DEV/Practice/GAMES/w4d1_battling_alien_game/battling.js
+++ OneDrive/Bureau/RTT25/DEV/Practice/GAMES/w4d1_battling_alien_game/battling.js
@@ -4,13 +4,13 @@
 //     userResponse: "",
 // };
 // RANDOM NUMBERS
-const randomHullAndFirepower = (min, max) =>{
-    return Math.floor(Math.random()*(max-min + 1) + min);
-    }
+// const randomHullAndFirepower = (min, max) =>{
+//     return Math.floor(Math.random()*(max-min + 1) + min);
+//     }
     
-    const randomAccuracy = (min, max) =>{
-        return Number(((Math.random() * (max - min) + min) / 10).toFixed(2));
-    }
+//     const randomAccuracy = (min, max) =>{
+//         return Number(((Math.random() * (max - min) + min) / 10).toFixed(2));
+//     }
     
     // THE SHIPS
     const USSHelloWorld = {
@@ -48,56 +48,59 @@ const randomHullAndFirepower = (min, max) =>{
     // };
     
         
-    const alienShips = {
-        hull: randomHullAndFirepower(3, 6),
-        firepower: randomHullAndFirepower(2, 4),
-        accuracy: randomAccuracy(6, 8)
-    };
+    // const alienShips = {
+    //     hull: randomHullAndFirepower(3, 6),
+    //     firepower: randomHullAndFirepower(2, 4),
+    //     accuracy: randomAccuracy(6, 8)
+    // };
     
     // GAME START
-    let flag = true;
-    while(flag){
-        const action = prompt("Your turn to act. Attack(A), Retreat (R)? (Enter s to stop/quit the game).") 
-    if(action.toLowerCase()==="a"){
-        // hull for both
-        if(USSHelloWorld.hull > 0 && alienShips.hull>0){
-            if(USSHelloWorld.accuracy > Math.random()){
-                alienShips.hull = alienShips - USSHelloWorld.firepower;
-                console.log("Great job! Target is hit", alienShips);
-            } else {
-                if(alienShips.accuracy > Math.random()){
-                    USSHelloWorld.hull = USSHelloWorld.hull - alienShips.firepower;
-                    console.log("You have been hit", USSHelloWorld);
-                }
-            }
-        // Check fighting state of the players
-        if(USSHelloWorld.hull <= 0 || alienShips.hull <= 0){
-            console.log("Not in health state of fighting");
-            break;
-            flag = false;
-            }  else {
-                console.log(USSHelloWorld);
-                console.log(alienShips);
-                continue;
-            }
-        }
-    
-       
-    } else if (action.toLocaleLowerCase() === "r"){
-        console.log("Retreat");
-        flag = false
-    } else if (action.toLocaleLowerCase() === "s"){
-        console.log("Stop/quit");
-        flag = false;
-        }
+let alienShips = [];
+const newAlien = () => {
+    const newalien = {
+
     }
+    newalien.hull = (Math.floor(Math.random() * (6-3)) + 3),
+    newalien.firepower = (Math.floor(Math.random() * (4-2)) + 2),
+    newalien.accuracy = (Math.random() * (.8 - .6) +.6).toFixed(2)
+
+    // console.log(newalien)
+    alienShips.push(newalien)
+}
+console.log(alienShips);
+newAlien()
+
+// console.log(alienShips);
+let buttonAttack = document.querySelector("button")
+buttonAttack.addEventListener('click', attack)
+
+function attack(){
+    if (USSHelloWorld.hull <= 0 || alienShips.hull <= 0){
+        return document.querySelector("button").disabled = true
+
+    } 
+    if (USSHelloWorld.accuracy > alienShips[0].accuracy){
+        USSHelloWorld.hull -= alienShips[0].firepower;
+        alienShips[0].hull -= USSHelloWorld.firepower;
+        console.log("Great, you hit the target!", alienShips);
+
+    } else if(USSHelloWorld.accuracy = alienShips[0].accuracy){
+        USSHelloWorld.hull -= alienShips[0].firepower;
+        alienShips[0].hull -= USSHelloWorld.firepower;
+        console.log("Oh no! You have been hit", USSHelloWorld)
+
+    }else{
+        USSHelloWorld.hull -= alienShips[0].firepower;
+        alienShips[0].hull -= USSHelloWorld.firepower;
+        console.log("Lucky you! You miss the target")
+    }
+}
+    
+    // console.log("Attack");
     
     
-    //console.log("Attack");
     
-    
-    
-    // console.log(USSHelloWorld, alienShips);
+    console.log(USSHelloWorld, alienShips[0]);
     
     
     
